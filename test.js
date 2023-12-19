63f661e566eaf5857881a958a3a707bbfde0164a
neeraj pawar <neerajpawar9893@gmail.com>
2023-12-19 16:27:19 +0530

    update test.js up to 3

--- test.js
+++ test.js
@@ -2,4 +2,16 @@ function test1(){
     console.log("hello")
 }
 
-test1()
+test2()
+
+function test1(){
+    console.log("hello2")
+}
+
+test2()
+
+function test3(){
+    console.log("hello3")
+}
+
+test3()
